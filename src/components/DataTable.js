4cba5bdd0f2db6c4f906d4ff38f63cc9b3868fb5
Jose Castro <jacastro023@gmail.com>
2020-02-19 21:04:40 -0600

    added components

---
+++ src/components/DataTable.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DataBody from "./DataBody";
+
+function DataTable({ users }) {
+    return (
+        <table className="table" id="employees">
+            <thead>
+                <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
+                </tr>
+            </thead>
+
+                <DataBody users={users}/>
+
+        </table>
+    );
+};
+
+export default DataTable;
